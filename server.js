74a099eab41876ec7e70cac40dcfa3ff56021df8
RoeiLevy <roeilevy99@gmail.com>
2023-08-29 13:49:43 +0300

    switch to env vars

--- server.js
+++ server.js
@@ -2,28 +2,19 @@ const express = require('express');
 const cors = require('cors');
 const { connectSockets } = require('./services/socket.service');
 
-// const path = require('path');
 const app = express();
 const http = require('http').createServer(app)
 const port = process.env.PORT || 8080;
 connectSockets(http);
 
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.resolve(__dirname, 'public')))
-// } else {
 const corsOptions = {
     origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://type-racer-sandy.vercel.app/'],
     credentials: true
 }
 app.use(cors(corsOptions))
-// }
 
 const api = require('./api/api.js')
 app.use('/api/', api)
-
-// app.get('/**', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public', 'index.html'))
-// })
 
 http.listen(port, () => {
     console.log('listening on http://localhost:8080');
--- services/db.service.js
+++ services/db.service.js
@@ -1,16 +1,17 @@
 
 const { MongoClient } = require('mongodb');
+require('dotenv').config(); 
 
-const config = require('../config')
-const dbName = 'typeRacer_db';
+const DB_URL = process.env.DB_URL
+const DB_NAME = 'typeRacer_db';
 var dbConn = null;
 
 async function connect() {
     if (dbConn) return dbConn;
     try {
-        const client = new MongoClient(config.dbURL);
+        const client = new MongoClient(DB_URL);
         const c = await client.connect()
-        const db = c.db(dbName);
+        const db = c.db(DB_NAME);
         dbConn = db;
         return db;
     } catch (err) {
