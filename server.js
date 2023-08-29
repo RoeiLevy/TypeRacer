864c5cbe2752d49c7dbe32de43d824c48f073eff
RoeiLevy <roeilevy99@gmail.com>
2023-08-29 13:18:43 +0300

    server fix

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const port = process.env.PORT || 8080;
 connectSockets(http);
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('public'))
+    app.use(express.static(path.resolve(__dirname, 'public')))
 } else {
     const corsOptions = {
-        origins: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://type-racer-sandy.vercel.app/'],
+        origin: ['http://127.0.0.1:3000', 'http://localhost:3000', 'https://type-racer-sandy.vercel.app/'],
         credentials: true
     }
     app.use(cors(corsOptions))
@@ -20,6 +20,10 @@ if (process.env.NODE_ENV === 'production') {
 
 const api = require('./api/api.js')
 app.use('/api/', api)
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
 
 http.listen(port, () => {
     console.log('listening on http://localhost:8080');
